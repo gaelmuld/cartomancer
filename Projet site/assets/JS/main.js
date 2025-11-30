0e5e874be256126d8b3c399bfda417b180547caa
Mulders Gael <ga3lmulders@gmail.com>
2025-11-30 21:20:26 +0100

    Projet actuel à date ( sans implémentation d'image)

---
+++ Projet site/assets/JS/main.js
@@ -0,0 +1,327 @@
+/* input: pour un appel au coup par coup
+    change: pour un appel une fois le focus fini
+*/
+function cl(...el) {
+  return console.log(...el)
+}
+
+function toDataURL(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    var reader = new FileReader();
+    reader.onloadend = function() {
+      callback(reader.result);
+    }
+    reader.readAsDataURL(xhr.response);
+  };
+  xhr.open('GET', url);
+  xhr.responseType = 'blob';
+  xhr.send();
+}
+
+toDataURL('https://www.gravatar.com/avatar/d50c83cc0c6523b4d3f6085295c953e0', function(dataUrl) {
+  //console.log('RESULT:', dataUrl)
+})
+
+var saveValues = function () {
+  values = {
+    "Titre": Titre,
+    "Info": {
+      "Info1": Info[1],
+      "Info2": Info[2],
+      "Info3": Info[3],
+      "Info4": Info[4],
+      "Info5": Info[5],
+      "Info6": Info[6]
+    }
+  }
+  return JSON.stringify(values)
+}
+
+var Info = []
+
+var values = saveValues()
+cl(values)
+
+/*Titre*/
+var Titre
+$("#cardTitre>h3").text($("#carteNameVal").val())
+$("#carteNameVal").on("input", function () {
+  $("#cardTitre>h3").text($("#carteNameVal").val())
+  Titre = $("#carteNameVal").val()
+  values = saveValues()
+});
+/*HP*/
+var Hp
+$("#cardInfo1").text($("#carteHpVal").val())
+$("#carteHpVal").on("input", function () {
+  $("#cardInfo1").text($("#carteHpVal").val())
+  Hp = $("#carteHpVal").val()
+  values = saveValues()
+});
+/*Mana*/
+var Mana
+$("#cardInfo2").text($("#carteManaVal").val())
+$("#carteManaVal").on("input", function () {
+  $("#cardInfo2").text($("#carteManaVal").val())
+  Hp = $("#carteManaVal").val()
+  values = saveValues()
+});
+
+/*Nombre d'attaque*/
+var nbAtk
+
+let val = $("#carteNbAtkVal").val()
+
+for (let i = 1; i <= 3; i++){
+  $("#carteAtk"+i+"Info").hide();
+  $("#atk"+i).hide();
+}
+
+for (let i = 1; i <= val; i++){
+  $("#carteAtk" + i + "Info").show();
+  $("#atk" + i).show();
+}
+
+$("#carteNbAtkVal").on("input", function () {
+  let val = $("#carteNbAtkVal").val()
+  for (let i = 1; i <= 3; i++) {
+    $("#carteAtk" + i + "Info").hide();
+    $("#atk" + i).hide();
+  }
+  for (let i = 1; i <= val; i++) {
+    $("#carteAtk" + i + "Info").show();
+    $("#atk" + i).show();
+    nbAtk = $("#carteManaVal").val()
+    values = saveValues()
+  }
+})
+
+/* Affichage des inputs par inputs */
+$(".fileOptionSelection").each(function (i) {
+  let $_this =$(this)
+  $_this.parent().next().hide()
+  $_this.on("input", function () {
+    $_this.parent().next().show()
+    $_this.parent().next().children(".fileOptionSelected2").hide()
+    $_this.parent().next().children(".fileOptionSelected1").hide()
+    if ($(this).val() == 0) {
+      $_this.parent().next().hide()
+    }
+    if ($(this).val() == 1) {
+      $_this.parent().next().children(".fileOptionSelected1").show()
+    }
+    if ($(this).val() == 2) {
+      $_this.parent().next().children(".fileOptionSelected2").show()
+    }
+  })
+})
+
+
+/*Liens atkInfo au éléments atk*/
+for (let i = 1; i <= 3; i++) {
+  $("#atk" + i + "Icone").html($("#atkIcone" + i + "Val").val());
+  $("#atkIcone" + i+"Val").on("input", function () {
+    let icone = $("#atkIcone" + i + "Val").val()
+    $("#atk" + i + "Icone").html($("#atkIcone" + i + "Val").val());
+  })
+  $("#atk" + i + "Name").html($("#atkName" + i + "Val").val());
+  $("#atkName" + i+"Val").on("input", function () {
+    let name = $("#atkName" + i + "Val").val()
+    $("#atk" + i + "Name").html($("#atkName" + i + "Val").val());
+  })
+  $("#atk" + i + "Power").html($("#atkPower" + i + "Val").val());
+  $("#atkPower" + i + "Val").on("input", function () {
+    let damage = $("#atkPower" + i + "Val").val()
+    $("#atk" + i + "Power").html($("#atkPower" + i + "Val").val());
+  })
+  $("#atk" + i + "Mana").html($("#atkMana" + i + "Val").val());
+  $("#atkMana" + i + "Val").on("input", function () {
+    let mana = $("#atkMana" + i + "Val").val()
+    $("#atk" + i + "Mana").html($("#atkMana" + i + "Val").val());
+  })
+  $("#atk" + i + "Desc").html($("#atkDesc" + i + "Val").val());
+  $("#atkDesc" + i + "Val").on("input", function () {
+    let description = $("#atkDesc" + i+"Val").val()
+  $("#atk" + i + "Desc").html($("#atkDesc" + i + "Val").val());
+  })
+}
+
+/*FlavorText*/
+var flavorText
+$("#cardInfo4").text($("#carteFlavorText").val())
+$("#carteFlavorText").on("input", function () {
+  flavorText = $("#carteFlavorText").val()
+  $("#cardInfo4").text($("#carteFlavorText").val())
+  values = saveValues()
+});
+
+var defaults
+if (localStorage["Values"]) {
+  defaults = JSON.parse(localStorage.getItem("Values"))
+  defaults
+}
+
+$("#SaveButton").click(function () {
+  defaults = JSON.parse(values)
+  localStorage.setItem("Values", values)
+})
+
+$("#LoadButton").click(function () {
+  if (localStorage["Values"]) {
+    defaults = JSON.parse(localStorage.getItem("Values"))
+    Titre = defaults["Titre"]
+    $("#cardTitle>h2").text(Titre)
+    for (let i = 0; i < 7; ++i) {
+      Info[i] = defaults.Info["Info" + i]
+      $("#cardInfo" + i).text(Info[i])
+    }
+  }
+})
+
+/**
+ * Code pour l'incrustation d'image - BORDURE
+ */
+$("#encadrement").on("input", function () {
+  let value = $("#encadrement").val()
+  if (value) {
+    $("#inside").css("border-image-source", "url('./assets/Ressources/" + value + "')");
+  } else {
+    $("#inside").css("border-image-source", "none");
+  }
+})
+
+/**
+ * Code pour choisir le nombre de champs
+ */
+$("#numberOption").on("input", function () {
+  let value = $("#numberOption").val()
+  if (value) {
+    for (let i = 1; i <= value; i++){
+      $("#Info"+i).css("visibility","visible")
+      $("#cardInfo"+i).css("visibility","visible")
+    }
+    value++
+    for (let i = value; i < 6; i++) {
+      $("#Info" + i).css("visibility", "hidden")
+      $("#cardInfo" + i).css("visibility", "hidden")
+    }
+    }
+})
+/**
+ * Code pour inclure une image d'illustration
+ */
+$("#illustration").on("input", function () {
+  let value = $("#illustration").val()
+  cl(value)
+  if (value) {
+    $("#cardIllustration").css("background-image", "url('./assets/Ressources/" + value + "')");
+  } else {
+    $("#cardIllustration").css("background-image", "none");
+  }
+})
+/**
+ * de/affichage du prop et de ses options
+ * [DEBUG] mettre tout en relative
+ */
+$("#placeProp").on("click", function () {
+  if (this.checked) {
+    $("#propIllustration").css("display", "block")
+    $("#displacement").css("display", "block")
+  } else {
+
+    $("#propIllustration").css("display", "none")
+    $("#displacement").css("display", "none")
+  }
+})
+//horizontal
+$("#horizontalRange").on("input", function () {
+  $("#propIllustration").css("left", "calc(50% + "+$("#horizontalRange").val()+"%)")
+})
+//vertical
+$("#verticalRange").on("input", function () {
+  $("#propIllustration").css("top", "calc(50% + "+$("#verticalRange").val()+"%)")
+})
+//scale
+$("#scaleRange").on("input", function () {
+  $("#propIllustration").css("width", ($("#scaleRange").val()*50) + "px")
+})
+//rotate
+$("#rotateValue").on("input", function () {
+  $("#propIllustration").css("transform", "rotate(" + $("#rotateValue").val()+"deg)")
+  })
+  /**
+   * DEBUG Mode
+  */
+$("#debugMode").on("click", function () {
+  if (this.checked) {
+    $(".field").css("outline-width", "3px")
+    $("#cardText").css("outline-width", "3px")
+    $("#cardText>div").css("outline-width", "3px")
+    $("#cardValues").css("outline-width", "3px")
+  } else {
+
+    $(".field").css("outline-width", "0px")
+    $("#cardText").css("outline-width", "0px")
+    $("#cardText").css("outline-width", "0px")
+    $("#cardText>div").css("outline-width", "0px")
+    $("#cardValues").css("outline-width", "0px")
+  }
+})
+/**PAR DÉFAUT */
+     $(".field").css("outline-width", "0px")
+     $("#cardText").css("outline-width", "0px")
+     $("#cardText").css("outline-width", "0px")
+     $("#cardText>div").css("outline-width", "0px")
+     $("#cardValues").css("outline-width", "0px")
+/** 
+ * Structure JSON d'une carte
+ */
+
+/*
+[{
+  "card1":
+  {
+    "id": [ID,"required"],
+    "title": [Title,"required"],
+    "pv":  [Hp,""],
+    "attack": [Attack,""],
+    "mana": [Mana,""],
+    "illustration": [Illus,"required"],
+    "border": [Border,"required"],
+    "background": [Bg,"required"],
+    "cardBackground": [CardBg,"required"],
+    "attack1": [{
+      "icon": [IcoAtt1,""],
+      "name": [NameAtt1,"required"],
+      "damage": [DamageAtt1,""],
+      "mana": [ManaAtt1,""],
+      "description": [DescAtt1,""]
+    },""],
+    "attack2": [{
+      "icon": [IcoAtt2,""],
+      "name": [NameAtt2,"required"],
+      "damage": [DamageAtt2,""],
+      "mana": [ManaAtt2,""],
+      "description": [DescAtt2,""]
+    },""],
+    "attack3": [{
+      "icon": [IcoAtt3,""],
+      "name": [NameAtt3,"required"],
+      "damage": [DamageAtt3,""],
+      "mana": [ManaAtt3,""],
+      "description": [DescAtt3,""]
+    },""],
+    "flavorText": [FlavText,""],
+    "credits": [Credits,""]
+  }
+}]
+
+
+//**
+// Image -> Base64
+//  */
+
+//**
+// Base64 -> Image
+//  *
